66e3273b02fd0be5bf8d5900bc4f7b3aaf020daf
edamame <gunma.u.robo@gmail.com>
2016-10-30 15:51:50 +0900

    カメラに5秒待機を追加.

--- camera.sh
+++ camera.sh
@@ -4,6 +4,6 @@
 #シャッター音
 aplay camera-shutter1.wav
 #写真撮影
-raspistill -o /home/pi/Pictures/clubroom.jpg -t 1
+raspistill -o /home/pi/Pictures/clubroom.jpg
 #写真加工
 python /home/pi/Pictures/face_over_write.py
